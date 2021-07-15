58f73226bd0d3c30fe402ead1434a3a86139d069
“jys” <runnaway@naver.com>
2021-07-15 20:33:45 +0900

    get thumbnail photo admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -86,4 +87,9 @@ class RoomAdmin(admin.ModelAdmin):
 class PhotoAdmin(admin.ModelAdmin):
     """Photo Admin Definition"""
 
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img src="{obj.file.url}" width="50"/>')
+
+    get_thumbnail.short_description = "thumbNail"

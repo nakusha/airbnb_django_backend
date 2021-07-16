6afe0f9208f7781fe83d4fe4cd6a2dd85e12c6f8
“jys” <runnaway@naver.com>
2021-07-16 11:17:46 +0900

    admin save

--- rooms/admin.py
+++ rooms/admin.py
@@ -29,7 +29,7 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {"fields": ("name", "description", "country", "city", "address", "price")},
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
@@ -78,6 +78,10 @@ class RoomAdmin(admin.ModelAdmin):
         "facilities",
         "house_rule",
     )
+
+    def save_model(self, request, obj, form, change):
+        print(obj, change)
+        return super().save_model(request, obj, form, change)
 
     # self=admin class/ object=선택된 줄
     def count_amenities(self, object):
